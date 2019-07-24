fcb099bfd2fa01d82414bf30212eceb65f699d4b
Ahamed Zubair <axz56040@ucmo.edu>
2019-07-24 15:58:13 -0500

    Added files to the repo

---
+++ practice-test/src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[]= [
+    {id: 11, name: 'Ahamed'},
+    {id: 12, name: 'Zubair'},
+    {id: 13, name: 'Anita'},
+    {id: 14, name: 'Shrestha'},
+    {id: 15, name: 'Optimus'},
+    {id: 16, name: 'Bumblee'},
+    {id: 17, name: 'Bee'},
+    {id: 18, name: 'Iron'},
+    {id: 19, name: 'Scott'},
+    {id: 20, name: 'Tornado'},
+    {id: 21, name: 'Lucy'},
+    {id: 22, name: 'GT'},
+]
