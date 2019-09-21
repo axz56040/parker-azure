80de639533d28ea8f7693da5a5fb8d8b854b5b29
Ahamed Zubair <axz56040@ucmo.edu>
2019-09-21 00:08:26 -0500

    Adding new Components

--- practice-test/src/app/heroes/heroes.component.ts
+++ practice-test/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
+import { HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,19 +10,19 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+ 
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
   onSlecet(hero: Hero): void{
     this.selectedHero = hero;
   }
-
-  constructor() { }
+getHeroes(): void{
+  this.heroService.getHeroes()
+  .subscribe(heroes => this.heroes = heroes);
+}
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     
